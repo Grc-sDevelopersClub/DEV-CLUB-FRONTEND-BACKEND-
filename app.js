ff9091379c1e1d3255cbf9c8d711f2add622cc5e
chavangorakh1999 <chavangorakh19999@gmail.com>
2020-11-14 17:20:37 +0530

    404 page added

--- app.js
+++ app.js
@@ -474,6 +474,26 @@ app.get("/donation", (req, res) => {
 
   
 // });
+
+app.use(function(req, res, next){
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render("404");
+    return;
+  }
+
+  // // respond with json
+  // if (req.accepts('json')) {
+  //   res.send({ error: 'Not found' });
+  //   return;
+  // }
+
+  // // default to plain-text. send()
+  // res.type('txt').send('Not found');
+});
+
 let port = process.env.PORT || 3000;
 
 app.listen(port, (req, res) => {
